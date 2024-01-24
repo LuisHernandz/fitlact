1712791a5002da190daa7e53341d0abe2b12df08
john099 <moralesgomezjohnny@gmail.com>
2024-01-24 00:19:55 -0600

    Cambio de vistas

--- FitLact/app/Http/Controllers/ViewsController.php
+++ FitLact/app/Http/Controllers/ViewsController.php
@@ -38,4 +38,17 @@ class ViewsController extends Controller
     public function index_alimentacion(){
         return view('empezar.alimentacion'); 
     } 
+
+    //SERVICIOS
+    public function nutricion(){
+        return view('service.nutricion');
+    }
+
+    public function productos(){
+        return view('service.productos');
+    }
+
+    public function salud(){
+        return view('service.salud');
+    }
 }
